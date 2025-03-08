f1d3c6dd192f22c5cb2220cbc5a004a25712fd32
David <davvan@pdx.edu>
2025-03-08 14:03:40 -0800

    Search bar filter bar cards

--- app/src/main/java/com/example/a30days/MainActivity.kt
+++ app/src/main/java/com/example/a30days/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,17 +27,23 @@ import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -45,6 +53,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -53,6 +62,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.a30days.data.Workout
+import com.example.a30days.data.WorkoutCategory
 import com.example.a30days.data.WorkoutRepository
 import com.example.compose._30DaysTheme
 
@@ -71,37 +81,121 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(
-    name="Dark Theme",
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-
-)
 
 @Composable
-fun WorkoutCardPreview() {
+fun WorkoutList(workouts: List<Workout>) {
+    var searchQuery by remember { mutableStateOf("") }
+    var selectedCategory by remember { mutableStateOf(WorkoutCategory.ALL)}
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.statusBars.asPaddingValues())
+    ) {
+        WorkoutSearchBar(
+            searchQuery = searchQuery,
+            onSearchQueryChange = { searchQuery = it }
+        )
+
+        WorkoutCategoryChips(
+            selectedCategory = selectedCategory,
+            onCategorySelect = { selectedCategory = it}
+        )
+
+        val filteredWorkouts = workouts.filter {
+            (selectedCategory == WorkoutCategory.ALL|| it.category == selectedCategory) &&
+                    (searchQuery.isBlank() || stringResource(it.nameRes).contains(searchQuery, ignoreCase = true))
+        }
+        Spacer(modifier = Modifier
+            .height(8.dp))
+
+        LazyColumn {
+            items(filteredWorkouts, key = { it.nameRes }) { workout ->
+                WorkoutCard(workout = workout)
+            }
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+fun WorkoutSearchBarPreview() {
     _30DaysTheme {
-        WorkoutCard(
-            Workout(R.string.treadmill, R.drawable.running, R.array.treadmill_tips)
+        Surface()
+        {
+            WorkoutList(WorkoutRepository.workouts)
+        }
+    }
+}
+
+@Composable
+fun WorkoutSearchBar(searchQuery: String, onSearchQueryChange: (String) -> Unit) {
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            label = { Text("Search Workouts")},
+            singleLine = true,
+            shape = MaterialTheme.shapes.medium,
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search")},
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surfaceVariant, // Background when focused
+                unfocusedContainerColor = MaterialTheme.colorScheme.surfaceVariant, // Background when not focused
+                focusedIndicatorColor = MaterialTheme.colorScheme.primary, // Border when focused
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurfaceVariant, // Border when not focused
+                cursorColor = MaterialTheme.colorScheme.primary, // Cursor color
+                focusedTextColor = MaterialTheme.colorScheme.onSurface,
+                unfocusedTextColor = MaterialTheme.colorScheme.onSurface
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         )
     }
 }
 
 
 @Composable
-fun WorkoutList(workouts: List<Workout>) {
-    Column(
+fun WorkoutCategoryChips(selectedCategory: WorkoutCategory, onCategorySelect: (WorkoutCategory) -> Unit) {
+    Card (
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         modifier = Modifier
-            .fillMaxSize()
-            .padding(WindowInsets.statusBars.asPaddingValues())
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 4.dp)
+    ){
+        LazyRow (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            items(WorkoutCategory.entries) { category ->
+                FilterChip(
+                    selected = category == selectedCategory,
+                    onClick = { onCategorySelect(category) },
+                    label = { Text(category.displayName) },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = FilterChipDefaults.filterChipColors(
+                        selectedContainerColor = MaterialTheme.colorScheme.primary,
+                        selectedLabelColor = MaterialTheme.colorScheme.onPrimary,
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        labelColor = MaterialTheme.colorScheme.onSurface
+                    )
 
-    ) {
-        LazyColumn {
-            items(workouts, key = { it.nameRes }) { workout ->
-                WorkoutCard(workout = workout)
+                )
             }
         }
     }
+
 
 }
 
--- app/src/main/java/com/example/a30days/data/Workout.kt
+++ app/src/main/java/com/example/a30days/data/Workout.kt
@@ -7,14 +7,25 @@ import com.example.a30days.R
 data class Workout(
     @StringRes val nameRes: Int,
     @DrawableRes val imageRes: Int,
-    val tipsArrayRes: Int
+    val tipsArrayRes: Int,
+    val category: WorkoutCategory
 )
 
 object WorkoutRepository {
     val workouts = listOf(
-        Workout(R.string.treadmill, R.drawable.running, R.array.treadmill_tips),
-        Workout(R.string.bench_press, R.drawable.bench_press, R.array.bench_press_tips),
-        Workout(R.string.squat, R.drawable.squat, R.array.squat_tips),
-        Workout(R.string.stationary_biking, R.drawable.stationary_bike, R.array.stationary_biking_tips),
+        Workout(R.string.treadmill, R.drawable.running, R.array.treadmill_tips, WorkoutCategory.CARDIO),
+        Workout(R.string.bench_press, R.drawable.bench_press, R.array.bench_press_tips, WorkoutCategory.CHEST),
+        Workout(R.string.squat, R.drawable.squat, R.array.squat_tips, WorkoutCategory.LEGS),
+        Workout(R.string.stationary_biking, R.drawable.stationary_bike, R.array.stationary_biking_tips, WorkoutCategory.CARDIO),
     )
+}
+
+enum class WorkoutCategory(val displayName: String) {
+    ALL("All"),
+    CARDIO("Cardio"),
+    CHEST("Chest"),
+    LEGS("Legs"),
+    FLEXIBILITY("Flexibility"),
+    STRENGTH("Strength"),
+    ENDURANCE("Endurance")
 }
